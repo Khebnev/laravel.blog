58a7eb8aff71d0a605960e76a41d4bfc80ca5a66
Anton <khebnev.a.v@gmail.com>
2022-08-05 19:56:26 +0300

    Перемигрировали таблицы, добавив для slug unique метод. Создал ресурс CategoryController. Прописал маршрут CategoryController. Скопировал card-body с демки AdminLTE 3 в index.blade категорий.Добавил пагинацию в CategoryController. Скопировал часть верстки из готового урока лектора, т.к. верстку он делал за экраном себе в верстку в шаблоны

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -153,67 +153,55 @@
       <span class="brand-text font-weight-light">На сайт</span>
     </a>
 
-    <!-- Sidebar -->
-    <div class="sidebar">
+     <!-- Sidebar -->
+     <div class="sidebar">
       <!-- Sidebar user (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="{{ asset('assets/admin/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
-        </div>
+          <div class="image">
+              <img src="{{ asset('assets/admin/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+              <a href="#" class="d-block">Alexander Pierce</a>
+          </div>
       </div>
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-         
-          <li class="nav-item">
-            <a href="{{ route('admin.index') }}" class="nav-link">
-              <i class="nav-icon fas fa-home"></i>
-              <p>Главная</p>
-            </a>
-          </li>
-
-          <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
               <li class="nav-item">
-                <a href="../../index.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
-                </a>
+                  <a href="{{ route('admin.index') }}" class="nav-link">
+                      <i class="nav-icon fas fa-home"></i>
+                      <p>Главная</p>
+                  </a>
               </li>
-              <li class="nav-item">
-                <a href="../../index2.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
-                </a>
+              <li class="nav-item has-treeview">
+                  <a href="#" class="nav-link">
+                      <i class="nav-icon fas fa-archive"></i>
+                      <p>
+                          Категории
+                          <i class="right fas fa-angle-left"></i>
+                      </p>
+                  </a>
+                  <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                          <a href="{{ route('categories.index') }}" class="nav-link">
+                              <i class="far fa-circle nav-icon"></i>
+                              <p>Список категорий</p>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="{{ route('categories.create') }}" class="nav-link">
+                              <i class="far fa-circle nav-icon"></i>
+                              <p>Новая категория</p>
+                          </a>
+                      </li>
+                  </ul>
               </li>
-              <li class="nav-item">
-                <a href="../../index3.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-
-
-        </ul>
+          </ul>
       </nav>
       <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
+      </div>
+  <!-- /.sidebar -->
   </aside>
 
   @yield('content')
@@ -236,5 +224,15 @@
 
 
 <script src="{{ asset('assets/admin/js/admin.js') }}"></script>
+<script>
+  $('.nav-sidebar a').each(function(){
+      let location = window.location.protocol + '//' + window.location.host + window.location.pathname;
+      let link = this.href;
+      if(link == location){
+          $(this).addClass('active');
+          $(this).closest('.has-treeview').addClass('menu-open');
+      }
+  });
+</script>
 </body>
 </html>
