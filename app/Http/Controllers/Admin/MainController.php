4f0b73473f0e3112406d0f7a29c8aae855b414f1
Anton <khebnev.a.v@gmail.com>
2022-08-05 16:38:59 +0300

    определили связи , установили пакет laravel-sluggable, прописали в моделях необходимый метод sluggable

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Tag;
 
 class MainController extends Controller
 {
     public function index()
     {
+        
+        $tag = new Tag();
+        $tag->title = 'Привет мир';
+        $tag->save();
         return view('admin.index');
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
+    use Sluggable;
     use HasFactory;
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsToMany(Category::class);
+    }
+      /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
