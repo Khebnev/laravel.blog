753a508ed610c8b92bbc684e8a4d11957369c010
Anton <khebnev.a.v@gmail.com>
2022-08-06 10:29:40 +0300

    Исправил опечатку, где в category.index.blade  добавил код с layout. Добавил шаблон admin.create и перенёс туда форму подготовленную лектором из материалов его урока

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -21,79 +21,79 @@
       </div><!-- /.container-fluid -->
     </section>
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Список категорий</h3>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <a href="{{ route('categories.create') }}" class="btn btn-primary mb-3">Добавить
-                                категорию</a>
-                            @if (count($categories))
-                                <div class="table-responsive">
-                                    <table class="table table-bordered table-hover text-nowrap">
-                                        <thead>
-                                        <tr>
-                                            <th style="width: 30px">#</th>
-                                            <th>Наименование</th>
-                                            <th>Slug</th>
-                                            <th>Actions</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        @foreach($categories as $category)
-                                            <tr>
-                                                <td>{{ $category->id }}</td>
-                                                <td>{{ $category->title }}</td>
-                                                <td>{{ $category->slug }}</td>
-                                                <td>
-                                                    <a href="{{ route('categories.edit', ['category' => $category->id]) }}" class="btn btn-info btn-sm float-left mr-1">
-                                                        <i class="fas fa-pencil-alt"></i>
-                                                    </a>
-
-                                                    <form action="{{ route('categories.destroy', ['category' => $category->id]) }}" method="post" class="float-left">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button type="submit" class="btn btn-danger btn-sm"
-                                                                onclick="return confirm('Подтвердите удаление')">
-                                                            <i
-                                                                class="fas fa-trash-alt"></i>
-                                                        </button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        </tbody>
-                                    </table>
-                                </div>
-                            @else
-                                <p>Категорий пока нет...</p>
-                            @endif
-                        </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer clearfix">
-                            {{ $categories->links() }}
-                            {{--<ul class="pagination pagination-sm m-0 float-right">
-                                <li class="page-item"><a class="page-link" href="#">«</a></li>
-                                <li class="page-item"><a class="page-link" href="#">1</a></li>
-                                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                <li class="page-item"><a class="page-link" href="#">»</a></li>
-                            </ul>--}}
-                        </div>
+   <!-- Main content -->
+   <section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Список категорий</h3>
                     </div>
-                    <!-- /.card -->
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <a href="{{ route('categories.create') }}" class="btn btn-primary mb-3">Добавить
+                            категорию</a>
+                        @if (count($categories))
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover text-nowrap">
+                                    <thead>
+                                    <tr>
+                                        <th style="width: 30px">#</th>
+                                        <th>Наименование</th>
+                                        <th>Slug</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($categories as $category)
+                                        <tr>
+                                            <td>{{ $category->id }}</td>
+                                            <td>{{ $category->title }}</td>
+                                            <td>{{ $category->slug }}</td>
+                                            <td>
+                                                <a href="{{ route('categories.edit', ['category' => $category->id]) }}" class="btn btn-info btn-sm float-left mr-1">
+                                                    <i class="fas fa-pencil-alt"></i>
+                                                </a>
 
+                                                <form action="{{ route('categories.destroy', ['category' => $category->id]) }}" method="post" class="float-left">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger btn-sm"
+                                                            onclick="return confirm('Подтвердите удаление')">
+                                                        <i
+                                                            class="fas fa-trash-alt"></i>
+                                                    </button>
+                                                </form>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        @else
+                            <p>Категорий пока нет...</p>
+                        @endif
+                    </div>
+                    <!-- /.card-body -->
+                    <div class="card-footer clearfix">
+                        {{ $categories->links() }}
+                        {{--<ul class="pagination pagination-sm m-0 float-right">
+                            <li class="page-item"><a class="page-link" href="#">«</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">»</a></li>
+                        </ul>--}}
+                    </div>
                 </div>
-                <!-- /.col -->
+                <!-- /.card -->
+
             </div>
-            <!-- /.row -->
-        </div><!-- /.container-fluid -->
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
   </div>
