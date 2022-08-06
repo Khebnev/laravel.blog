646c7947e5ad88ebf4ec7b4f73c2ffd7c640165a
Anton <khebnev.a.v@gmail.com>
2022-08-06 14:21:26 +0300

    Изменил h1 в виде create с Главная на Создание категории

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -9,7 +9,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Главная</h1>
+            <h1>Создание категории</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
