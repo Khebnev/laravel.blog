90a5e86cde8986cbd66eea1722829d8d5409f087
Anton <khebnev.a.v@gmail.com>
2022-08-17 15:10:48 +0300

    Настроил вывод ссылки названия категории в списках постов на главной

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -57,7 +57,7 @@
                 <h4><a href="{{  route('posts.single', ['slug' => $post->slug]) }}" title="">{{ $post->title }}</a></h4>
                 {!! $post->description !!}
                 {{-- Как у лектора: --}}
-                {{-- <small><a href="{{ route('categories.single'), ['slug' => $post->category->slug] }}" title="">{{ $post->category->title }}</a></small>  --}}
+                <small><a href="{{ route('categories.single', ['slug' => $post->category->slug]) }}" title="">{{ $post->category->title }}</a></small> 
                 <small>{{ $post->getPostDate() }}</small>
                 <small><i class="fa fa-eye"></i>{{ $post->views }}</small>
             </div><!-- end meta -->
